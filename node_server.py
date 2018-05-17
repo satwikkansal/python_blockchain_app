313fdfda2e258c98d2e6bd04b410a68370f5dbb1
Satwik Kansal <satwikkansal@gmail.com>
2018-05-17 18:58:41 +0530

    * Make is_valid_proof a class method. * Add check_chain_validity method.

--- node_server.py
+++ node_server.py
@@ -58,20 +58,12 @@ class Blockchain:
         if previous_hash != block.previous_hash:
             return False
 
-        if not self.is_valid_proof(block, proof):
+        if not Blockchain.is_valid_proof(block, proof):
             return False
 
         block.hash = proof
         self.chain.append(block)
         return True
-
-    def is_valid_proof(self, block, block_hash):
-        """
-        Check if block_hash is valid hash of block and satisfies
-        the difficulty criteria.
-        """
-        return (block_hash.startswith('0' * Blockchain.difficulty) and
-                block_hash == block.compute_hash())
 
     def proof_of_work(self, block):
         """
@@ -89,6 +81,35 @@ class Blockchain:
 
     def add_new_transaction(self, transaction):
         self.unconfirmed_transactions.append(transaction)
+
+    @classmethod
+    def is_valid_proof(cls, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    @classmethod
+    def check_chain_validity(cls, chain):
+        result = True
+        previous_hash = "0"
+
+        for block in chain:
+            block_hash = block.hash
+            # remove the hash field to recompute the hash again
+            # using `compute_hash` method.
+            delattr(block, "hash")
+
+            if not cls.is_valid_proof(block, block.hash) or \
+                    previous_hash != block.previous_hash:
+                result = False
+                break
+
+            block.hash, previous_hash = block_hash, block_hash
+
+        return result
 
     def mine(self):
         """
@@ -213,7 +234,7 @@ def consensus():
     global blockchain
 
     longest_chain = None
-    current_len = len(blockchain)
+    current_len = len(blockchain.chain)
 
     for node in peers:
         response = requests.get('http://{}/chain'.format(node))
